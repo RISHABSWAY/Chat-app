af0a36d7a76f28f8f7c50cdd574d5b43345d6255
1amSumit <sj779619@gmail.com>
2024-03-26 22:09:28 +0530

    changes

--- front/src/Hooks/userSignup.js
+++ front/src/Hooks/userSignup.js
@@ -1,56 +1,92 @@
 import { useState } from "react";
-import toast from 'react-hot-toast'
+import toast from "react-hot-toast";
 import { useAuthContext } from "../Context/authContext";
 
-
 const useSignup = () => {
-	const [loading, setLoading] = useState(false);
-	const { setAuthUser } = useAuthContext();
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
-	const signup = async ({ Email, Fullname, Username, password, confirmpassword, Gender }) => {
-		const success = handleInputErrors({ Email, Fullname, Username, password, confirmpassword, Gender });
-		if (!success) return;
+  const signup = async ({
+    Email = "",
+    Fullname = "",
+    Username = "",
+    password = "",
+    confirmpassword = "",
+    Gender = "",
+  }) => {
+    const success = handleInputErrors({
+      Email,
+      Fullname,
+      Username,
+      password,
+      confirmpassword,
+      Gender,
+    });
+    if (!success) return;
 
-		setLoading(true);
-		try {
-			const res = await fetch("http://localhost:5000/api/auth/signup", {
-				method: "POST",
-				headers: { "Content-Type": "application/json"},
-				body: JSON.stringify({ Email, Fullname, Username, password, confirmpassword, Gender }),
-			});
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          Email,
+          Fullname,
+          Username,
+          password,
+          confirmpassword,
+          Gender,
+        }),
+      });
 
-			const data = await res.json();
-			if (data.error) {
-				throw new Error(data.error);
-			}
-			localStorage.setItem("chat-user", JSON.stringify(data));
-			setAuthUser(data);
-		} catch (error) {
-			toast.error(error.message);
-		} finally {
-			setLoading(false);
-		}
-	};
+      const data = await res.json();
 
-	return { loading, signup };
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      localStorage.setItem("token", JSON.stringify(data.token));
+      setAuthUser(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
 };
 export default useSignup;
 
-function handleInputErrors({ Email, Fullname, Username, password, confirmpassword, Gender }) {
-	if (!Email || !Fullname || !Username || !password || !confirmpassword || !Gender) {
-		toast.error("Please fill in all fields");
-		return false;
-	}
+function handleInputErrors({
+  Email,
+  Fullname,
+  Username,
+  password,
+  confirmpassword,
+  Gender,
+}) {
+  if (
+    !Email ||
+    !Fullname ||
+    !Username ||
+    !password ||
+    !confirmpassword ||
+    !Gender
+  ) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
 
-	if (password !== confirmpassword) {
-		toast.error("Passwords do not match");
-		return false;
-	}
+  if (password !== confirmpassword) {
+    toast.error("Passwords do not match");
+    return false;
+  }
 
-	if (password.length < 6) {
-		toast.error("Password must be at least 6 characters");
-		return false;
-	}
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
 
-	return true;
+  return true;
 }
